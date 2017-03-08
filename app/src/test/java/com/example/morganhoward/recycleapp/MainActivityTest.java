50b180f5674e0a758213b534e91cb763891752c9
Morgan Howard <morganhoward@users.noreply.github.com>
2017-03-07 17:58:33 -0800

    init

---
+++ app/src/test/java/com/example/morganhoward/recycleapp/MainActivityTest.java
@@ -0,0 +1,16 @@
+package com.example.morganhoward.recycleapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by morganhoward on 3/7/17.
+ */
+public class MainActivityTest {
+    @Test
+    public void goto_product() throws Exception {
+
+    }
+
+}
